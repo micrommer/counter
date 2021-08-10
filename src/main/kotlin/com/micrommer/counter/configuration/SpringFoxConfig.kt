c4df2babbe3c94c271fbb2b6103c454ce04bb0e4
ibhlool7 <ibhlool7@gmail.com>
2021-08-10 14:59:40 +0430

    Add Swagger Contact

--- src/main/kotlin/com/micrommer/counter/configuration/SpringFoxConfig.kt
+++ src/main/kotlin/com/micrommer/counter/configuration/SpringFoxConfig.kt
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
+import java.util.*
+
 
 /**
  * counter (com.micrommer.counter.configuration)
@@ -22,5 +26,16 @@ class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
+                .apiInfo(apiInfo())
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfo(
+                "Smart Counter",
+                "A way to communicate with Smart Counter securely",
+                "v 0.1",
+                "",
+                Contact("Iman Buhlool", "", "ibhlool7@gmail.com"),
+                "", "", Collections.emptyList())
     }
 }
