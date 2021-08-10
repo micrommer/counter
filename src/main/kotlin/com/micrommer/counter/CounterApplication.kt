de0eca7f1a821732097bc4037f4e3c109f3a54ad
ibhlool7 <ibhlool7@gmail.com>
2021-08-10 14:51:02 +0430

    .gitignore is now working

--- src/main/kotlin/com/micrommer/counter/CounterApplication.kt
+++ src/main/kotlin/com/micrommer/counter/CounterApplication.kt
@@ -2,6 +2,18 @@ package com.micrommer.counter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+
+import springfox.documentation.builders.RequestHandlerSelectors
+
+import springfox.documentation.spi.DocumentationType
+
+import springfox.documentation.spring.web.plugins.Docket
+
+
+
 
 @SpringBootApplication
 class CounterApplication
---
+++ src/main/kotlin/com/micrommer/counter/configuration/SpringFoxConfig.kt
@@ -0,0 +1,26 @@
+package com.micrommer.counter.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+/**
+ * counter (com.micrommer.counter.configuration)
+ * @author : imanbhlool
+ * @since : Aug/10/2021 - 2:39 PM, Tuesday
+ */
+
+@Configuration
+class SpringFoxConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+    }
+}
