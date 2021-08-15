a94e1c471d0bbc4db058232040bba482bc31759e
ibhlool7 <ibhlool7@gmail.com>
2021-08-16 02:13:22 +0430

    Implement Notifier

--- src/main/kotlin/com/micrommer/counter/service/EmailNotifier.kt
+++ src/main/kotlin/com/micrommer/counter/service/EmailNotifier.kt
@@ -5,7 +5,12 @@ import org.springframework.stereotype.Service
 
 @Service
 class EmailNotifier : Notifier {
+    private val color = "\u001B[34m"
+    private val reset = "\u001B[0m"
     override fun notify(recipients: List<String>, message: String) {
-
+        println("$color------------------------------------------------------------------------------")
+        println("recipients : $recipients")
+        println(message)
+        println("------------------------------------------------------------------------------$reset")
     }
 }
