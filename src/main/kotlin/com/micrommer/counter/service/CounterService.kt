d05b0ffd4d7b0517cf531c7fd8b8704c6506b87b
ibhlool7 <ibhlool7@gmail.com>
2021-08-15 13:15:18 +0430

    Add some notify

--- src/main/kotlin/com/micrommer/counter/service/CounterService.kt
+++ src/main/kotlin/com/micrommer/counter/service/CounterService.kt
@@ -1,6 +1,5 @@
 package com.micrommer.counter.service
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.micrommer.counter.model.dao.CounterDao
 import com.micrommer.counter.model.dao.RecordDao
 import com.micrommer.counter.model.dto.CounterDto
@@ -8,6 +7,7 @@ import com.micrommer.counter.model.dto.RecordDto
 import com.micrommer.counter.repo.CounterRepo
 import com.micrommer.counter.repo.abstraction.ManualCounterRepo
 import org.bson.types.ObjectId
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
@@ -16,28 +16,40 @@ import org.springframework.stereotype.Service
 class CounterService(
         private val counterRepo: CounterRepo,
         private val manualCounterRepo: ManualCounterRepo,
-        private val notifier: EmailNotifier,
-        private val om: ObjectMapper
+        private val notifier: EmailNotifier
 ) {
+    @Value("\${department.top-level.email}")
+    lateinit var topLevelDepartmentEmail: String
+
     enum class MessageTopic {
-        INACTIVE_RECEIVED
+        INACTIVE_RECEIVED, DOES_NOT_EXIST_RECEIVED, ZERO_CONSUMPTION
     }
 
     /**
-     * Add Counter Record: Adds new record to <em> active </em> counter
+     * Add Counter Record: Adds new record to a counter, if it is not active, record will committed and owners will
+     * notified and if counter doesn't exist, top level department will notified.
+     * In case of receiving 0 consumption from an active Counter, we well notified owners.
      */
     fun addCounterRecord(record: RecordDto) {
 
         val entity = counterRepo.findById(record.counterId)
         if (entity.isPresent) {
-            val presentEntity = entity.get()
-            if (!presentEntity.active) {
+            if (!entity.get().active) {
                 notifier.notify(
-                        presentEntity.owners.map { owner -> return@map owner.email },
+                        entity.get().owners.map { owner -> return@map owner.email },
                         this.messageGenerator(MessageTopic.INACTIVE_RECEIVED)
                 )
+            } else {
+                if (record.consumption == 0.0) {
+                    notifier.notify(
+                            entity.get().owners.map { owner -> return@map owner.email },
+                            this.messageGenerator(MessageTopic.ZERO_CONSUMPTION)
+                    )
+                }
             }
             manualCounterRepo.addRecordToCounter(record.counterId, RecordDao(record.datetime, record.consumption, record.geoLocation))
+        } else {
+            notifier.notify(listOf(topLevelDepartmentEmail), messageGenerator(MessageTopic.DOES_NOT_EXIST_RECEIVED))
         }
     }
 
@@ -57,11 +69,28 @@ class CounterService(
     private fun messageGenerator(topic: MessageTopic) = when (topic) {
         MessageTopic.INACTIVE_RECEIVED -> {
             """
-                Hi,
+                Hello,
                 There is a problem with your Counter, we've received data from your Counter while your Counter is 
                 inactivated!
                 
-                Thanks
+                Best Regards
+            """.trimIndent()
+        }
+        MessageTopic.DOES_NOT_EXIST_RECEIVED -> {
+            """
+                Hello,
+                We have received a record from unknown Counter, please handle this case.
+                
+                Best Regards
+            """.trimIndent()
+        }
+        MessageTopic.ZERO_CONSUMPTION -> {
+            """
+                Hello,
+                We have received a zero record from your Counter, please check it out or in case of non-use, explicitly
+                disable it.
+                
+                Best Regards
             """.trimIndent()
         }
     }
